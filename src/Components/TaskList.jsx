0cc779fa5e338fcf2f7b121af91f017e1cee4219
Rishi <rk6180442@gmail.com>
2025-01-21 21:14:51 +0530

    Added Edit Feature

--- src/Components/TaskList.jsx
+++ src/Components/TaskList.jsx
@@ -6,6 +6,9 @@ function TaskList(props) {
     const [index, setIndex] = useState(0);
     const [taskArray, setTaskArray] = useState([]);
 
+    const[isEditing , setIsEditing] = useState(null);
+    const[editedTask , setEditedTask] = useState("");
+
     return (
         <div>
             <button onClick={() => {
@@ -18,15 +21,35 @@ function TaskList(props) {
                         }
                     ]);
                     setIndex(index + 1);
+                    props.onTaskAdded();
                 }
             }}>Add Task</button>
 
             <ul>
                 {taskArray.map(tasks => (
                     <li key={tasks.id}>
-                        {tasks.name}
-                        <button onClick={() => {
-                            setTaskArray(taskArray.filter(task => task.id !== tasks.id));
+                        {isEditing === tasks.id?(
+                            <input type="text"
+                              value={editedTask}
+                              onChange={(e)=>setEditedTask(e.target.value)}
+                            />
+                        ):(
+                            <span>{tasks.name}</span>
+                        )}
+                        <button onClick={()=>{
+                            if(isEditing === tasks.id){
+                                if(editedTask.trim() !== ""){
+                                    setTaskArray(taskArray.map(t=> t.id===tasks.id?{...t,name:editedTask}:t));
+                                }
+                                setIsEditing(null);
+                            }else{
+                                setIsEditing(tasks.id)
+                                setEditedTask(t.name)
+                            }
+                        }}>{isEditing===tasks.id ? "Save" :"Edit"}</button>
+
+                        <button onClick={()=>{
+                            setTaskArray(taskArray.filter(t=>t.id !== tasks.id))
                         }}>Delete</button>
                     </li>
                 ))}
